fda87ee859aec404434fa6704d3b830fc96e48ab
Anh Pham Tuan <anh.phamtuan@nashtechglobal.com>
2022-11-19 13:49:38 +0700

    update

--- js/dangtintuc.js
+++ js/dangtintuc.js
@@ -1,22 +1,3 @@
-let progressBar = document.querySelector('.circular-progress');
-let valueContainer = document.querySelector('.value-container');
-
-let progressValue = 0;
-let progressEndValue = 30;
-let speed = 10;
-
-let progress = setInterval(() => {
-  progressValue++;
-  valueContainer.textContent = `${progressValue}%`;
-  progressBar.style.background = `conic-gradient(
-      #D1193F ${progressValue * 3.6}deg,
-      #ECEDF1 ${progressValue * 3.6}deg
-      )`;
-  if (progressValue == progressEndValue) {
-    clearInterval(progress);
-  }
-}, speed);
-
 // Count in BDS
 let countBedroomInit = 0;
 let countBathroomInit = 0;
@@ -63,4 +44,74 @@ document.querySelectorAll('.dangtintuc-image').forEach((item) => {
       .classList.remove('active');
     item.classList.add('active');
   };
+});
+
+//
+
+let progressBar = document.querySelector('.circular-progress');
+let valueContainer = document.querySelector('.value-container');
+let speed = 10;
+const getProgressEndValue = () => {
+  return Math.floor(
+    (100 / totalLength) *
+      document.querySelectorAll(
+        'input.ptanh-custom-input:not(:placeholder-shown):not([type="date"]), textarea:not(:placeholder-shown), .ptanh-custom-select.selected'
+      ).length
+  );
+};
+const changeProgress = () => {
+  const progressEndValue = getProgressEndValue();
+  let progressValue = valueContainer.textContent.split('%')[0];
+  let progress = setInterval(() => {
+    if (progressValue < progressEndValue) progressValue++;
+    else if (progressValue > progressEndValue) progressValue--;
+    else return;
+    valueContainer.textContent = `${progressValue}%`;
+    progressBar.style.background = `conic-gradient(
+        #D1193F ${progressValue * 3.6}deg,
+        #ECEDF1 ${progressValue * 3.6}deg
+        )`;
+    if (progressValue == progressEndValue) {
+      clearInterval(progress);
+    }
+  }, speed);
+};
+
+const totalLength = document.querySelectorAll(
+  'input.ptanh-custom-input:not([type="date"]), textarea, select.ptanh-custom-select'
+).length;
+
+$('.ptanh-custom-select').on(
+  'changed.bs.select',
+  function (e, clickedIndex, isSelected, previousValue) {
+    e.currentTarget.parentNode.classList.toggle(
+      'selected',
+      $(e.currentTarget).val()?.length !== 0
+    );
+    changeProgress();
+    const dependanceName = e.currentTarget.dataset.dependanceName;
+    if (dependanceName)
+      document
+        .querySelectorAll(
+          `select.ptanh-custom-select[data-dependance="${dependanceName}"]`
+        )
+        .forEach((item) => {
+          // $(item).selectpicker('deselectAll');
+          if (Array.isArray($(item).selectpicker('val')))
+            $(item).selectpicker('deselectAll');
+          else $(item).selectpicker('val', '');
+        });
+  }
+);
+document.querySelectorAll('.clear-select-bootstrap').forEach((item) => {
+  item.onclick = () => {
+    const select = item.previousElementSibling.querySelector('select');
+    $(select).selectpicker('val', '');
+  };
+});
+
+document.querySelectorAll('input, textarea').forEach((input) => {
+  input.addEventListener('change', () => {
+    changeProgress();
+  });
 });
