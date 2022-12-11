846e6ca869eb6f7cbb54dfd2946ca50e47f00677
Anh Pham Tuan <anh.phamtuan@nashtechglobal.com>
2022-12-11 17:37:11 +0700

    fox

--- js/index.js
+++ js/index.js
@@ -823,3 +823,10 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', function (event) {
 });
 
 //
+
+document.querySelectorAll('.switch').forEach((item) => {
+  console.log(item);
+  item.addEventListener('change', (e) => {
+    console.log(e.target.checked);
+  });
+});
